6ff81c53cf04e0c4f7c7a27ad31369c8c6f4c123
Eetu Manninen <eetutj@gmail.com>
2017-08-13 03:15:17 +0300

    parse request

--- backend/routes/adduser.js
+++ backend/routes/adduser.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-
-
 router.post('/', function(req, res){
-  console.log(req);
-  console.log(req.body)
+    var b64string = req.body;
+    var buf = new Buffer(b64string, 'base64');
+    var call = JSON.parse(buf);
+    console.log(call)
   /*canvas.save(function(err, result){
       if(err){
           return res.status(500).json({
