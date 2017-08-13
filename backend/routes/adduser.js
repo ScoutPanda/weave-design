436ec1de8d8237c5fbc569db0c02e5e4bc7effdc
Eetu Manninen <eetutj@gmail.com>
2017-08-13 05:05:48 +0300

    change get to post

--- backend/routes/adduser.js
+++ backend/routes/adduser.js
@@ -3,20 +3,17 @@ const router = express.Router();
 
 var User = require('../models/user');
 
-router.get('/', function(req, res){
-    var b64string = req.query.data;
+router.post('/', function(req, res){
+    var b64string = req.body;
     var buf = new Buffer(b64string, 'base64');
     var call = JSON.parse(buf.toString());
-    console.log("call" + call);
-    if(call.properties.secret === "{tosikovasalaisuus12345}"){
+    if(req.header("secret") === "{tosikovasalaisuus12345}"){
         var user = new User({
             authUserId: call.properties.distinct_id,
             email: call.properties.distinct_email
         });
-        console.log("inside if");
         user.save(function(err, result){
             if(err){
-                console.log(err);
             return res.status(500).json({
                 title: 'An error occured',
                 error: err
@@ -26,10 +23,8 @@ router.get('/', function(req, res){
             message: 'Saved user data',
             obj: result
             });
-            console.log(res)
         });
     }else{
-        console.log("unauthorized")
         res.status(403).json({
             title: 'UNAUTHORIZED',
             error: err
