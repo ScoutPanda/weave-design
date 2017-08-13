f797fee24a4818b63f60848b0056c91892a5d27e
Eetu Manninen <eetutj@gmail.com>
2017-08-13 03:46:35 +0300

    message

--- backend/routes/adduser.js
+++ backend/routes/adduser.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+var User = require('../models/user');
+
 router.post('/', function(req, res){
     var b64string = req.body;
     var buf = new Buffer(b64string, 'base64');
-    var call = JSON.parse(buf);
-    console.log(call)
-  /*canvas.save(function(err, result){
-      if(err){
-          return res.status(500).json({
-            title: 'An error occured',
+    var call = JSON.parse(buf.toString());
+    if(call.properties.secret === "{tosikovasalaisuus12345}"){
+        var user = new User({
+            authUserId: call.properties.distinct_id,
+            email: call.properties.distinct_email
+        });
+        user.save(function(err, result){
+            if(err){
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+            }
+            res.status(201).json({
+            message: 'Saved user data',
+            obj: result
+            });
+        });
+    }else{
+        res.status(403).json({
+            title: 'UNAUTHORIZED',
             error: err
-          });
-      }
-      res.status(201).json({
-          message: 'Saved canvas data',
-          obj: result
-      });
-  });*/
+        });
+    }
 });
 
 module.exports = router;
